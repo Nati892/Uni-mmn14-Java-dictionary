65e5d0539d6dcd6889bec441e9e0731c669921c3
Nati892 <netanel892@gmail.com>
2022-05-11 12:57:50 +0300

    Done
    Need to add ap docu

--- src/DictionaryController.java
+++ src/DictionaryController.java
@@ -10,12 +10,7 @@ import java.util.Map;
 
 public class DictionaryController {
 
-    private DictionaryMap currDictionary;
-    private File currFile = null;
-
-    private final String DB_Dir_PATH = (System.getProperty("user.dir") + "\\Db"); //the Db directory path
-    private final String DB_FILE_EXTENSION = ".mydb";//the db files extension
-
+    private DictionaryMapDb currDictionaryMapDb;
 
     @FXML
     private TableView TableView;
@@ -79,8 +74,10 @@ public class DictionaryController {
 
     @FXML
     void btnOpenOnClick(ActionEvent event) {
-        chooseFile();
-        UpdateCurrFileLabel();
+        if (currDictionaryMapDb.chooseFileFromDir()) {
+            repopulateDictionaryList();
+            UpdateCurrFileLabel();
+        }
     }
 
     @FXML
@@ -91,7 +88,10 @@ public class DictionaryController {
 
     @FXML
     void btnSaveOnClick(ActionEvent event) {
-        SaveTofile(currFile);
+        if (currDictionaryMapDb.getSelectedFileName()!=null)
+            currDictionaryMapDb.saveToCurrFile();
+        else
+            saveToNewFile();
         UpdateCurrFileLabel();
     }
 
@@ -103,13 +103,11 @@ public class DictionaryController {
 
     @FXML
     void initialize() {
+
         C1Key.setCellValueFactory(new PropertyValueFactory<>("key"));//setup cells
         C2Value.setCellValueFactory(new PropertyValueFactory<>("value"));
-        currDictionary = new DictionaryMap();
-        chooseInitialFile();//choose first db file from the files in the db folder
-        currDictionary = loadDictionaryFromFile(currFile);// load the dictionary from the file selected
-        if (currDictionary == null)
-            currDictionary = new DictionaryMap();
+        currDictionaryMapDb = DictionaryMapDb.getInstance();
+
         populateDictionaryList();
         UpdateCurrFileLabel();
 
@@ -117,16 +115,18 @@ public class DictionaryController {
 
 
     private void populateDictionaryList() {
-        if (currDictionary != null) {
-            for (Map.Entry<String, DictionaryEntry> entry : currDictionary.entrySet()) {
-                TableView.getItems().add(entry.getValue());
+        if (currDictionaryMapDb != null) {
+            if (currDictionaryMapDb.entrySet() != null) {
+                for (Map.Entry<String, DictionaryEntry> entry : currDictionaryMapDb.entrySet()) {
+                    TableView.getItems().add(entry.getValue());
+                }
             }
         }
 
     }
 
     private void repopulateDictionaryList() {//clear the table and populate it again
-        if (currDictionary != null) {
+        if (currDictionaryMapDb != null) {
             TableView.getItems().clear();
             populateDictionaryList();
         }
@@ -142,22 +142,15 @@ public class DictionaryController {
             JOptionPane.showMessageDialog(null, "Missing Value!");
             return;
         }
-        addToList(currDictionary, new DictionaryEntry(TFKey.getText().trim(), TFValue.getText().trim()));
-    }
-
-
-    private void addToList(DictionaryMap map, DictionaryEntry newEntry) {
-        if (map != null && newEntry != null) {
-            map.insert(newEntry);//insert a new object to the map and reload table
-            repopulateDictionaryList();
-        }
+        currDictionaryMapDb.addEntry(new DictionaryEntry(TFKey.getText().trim(), TFValue.getText().trim()));
+        repopulateDictionaryList();
     }
 
 
     private void removeFromList() {
         DictionaryEntry entry = (DictionaryEntry) TableView.getSelectionModel().getSelectedItem();
         if (entry != null) {
-            currDictionary.remove(entry.getKey());
+            currDictionaryMapDb.removeEntry(entry.getKey());
             repopulateDictionaryList();
         } else {
             JOptionPane.showMessageDialog(null, "Please select an entry");
@@ -169,52 +162,17 @@ public class DictionaryController {
     private void editEntryValue() {
         DictionaryEntry entry = (DictionaryEntry) TableView.getSelectionModel().getSelectedItem();
         if (entry != null) {
-            String newVal = JOptionPane.showInputDialog("Please enter a new value for '" + entry.getKey() + "'");
+            String newVal = JOptionPane.showInputDialog("Please enter a new value for '" + entry.getKey() + "'",entry.getValue());
             if (newVal == null)//in case of cancel
                 return;
 
             if (newVal.trim().length() != 0) {
-                currDictionary.getEntry(entry.getKey()).setValue(newVal);
+                currDictionaryMapDb.getEntry(entry.getKey()).setValue(newVal);
                 repopulateDictionaryList();
             }
         } else {
             JOptionPane.showMessageDialog(null, "Please select an entry");
-
-
         }
-
-    }
-
-
-    private DictionaryMap loadDictionaryFromFile(File file) {
-        DictionaryMap result = null; //the new dictionary map to work with
-
-        FileInputStream fileIn = null;
-        ObjectInputStream objectIn = null;
-        if (file != null) {
-            try {//try to open and read from the file
-                fileIn = new FileInputStream(file);
-                objectIn = new ObjectInputStream(fileIn);
-
-                result = (DictionaryMap) objectIn.readObject();
-
-            } catch (Exception ex) {
-                JOptionPane.showMessageDialog(null, "Cannot load Dictionary from file! file name: " + file.getName());
-                result = null;
-            } finally {
-
-                try {//try to close file related objects
-                    objectIn.close();
-                } catch (Exception e) {
-                }
-                try {
-                    fileIn.close();
-                } catch (Exception e) {
-                }
-            }
-        } else
-            result = null;
-        return result;
     }
 
 
@@ -223,92 +181,32 @@ public class DictionaryController {
         String file_name = JOptionPane.showInputDialog("please choose file name");
         if (file_name == null)
             return;
-        if (file_name.trim().length() > 0) {
-            File file = new File(DB_Dir_PATH + "\\" + file_name + DB_FILE_EXTENSION); //initializing File object, and passing path as argument
-            SaveTofile(file);
+        file_name = file_name.trim();
+        if (file_name.length() > 0) {
+            currDictionaryMapDb.SaveToNewFile(file_name);
         } else {
             JOptionPane.showMessageDialog(null, "Invalid file name");
         }
-
     }
 
-    private void SaveTofile(File file) {
-        if (file == null) {
-            saveToNewFile();
-            return;
-        }
-
-        try {
-            boolean result = file.createNewFile();  //creates a new file
-            FileOutputStream fos = new FileOutputStream(file);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(currDictionary);
-            oos.close();
-            currFile = file;
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Cannot save file!");
-        }
-    }
-
-    private void chooseFile() {
-
-        FileChooser fileChooser = new FileChooser();//using preset file chooser
-        fileChooser.setTitle("Choose Db File from this folder only!");
-
-        File userDirectory = new File(DB_Dir_PATH);
-        fileChooser.setInitialDirectory(userDirectory);
-        File selectedFile = fileChooser.showOpenDialog(null);
-        if (selectedFile != null) {
-            DictionaryMap map = loadDictionaryFromFile(selectedFile);
-            if (map != null) {
-                currFile = selectedFile;
-                currDictionary = map;
-                repopulateDictionaryList();
-            } else {
-                if (currDictionary == null)
-                    currDictionary = new DictionaryMap();
-            }
-        }
-    }
-
-    private void chooseInitialFile() {// called on startup, and chooses first file
-        File folder = new File(DB_Dir_PATH);
-        File[] listOfFiles = folder.listFiles();
-
-        File iFile = null;
-        boolean foundOne = false;
-
-        for (int i = 0; i < listOfFiles.length && !foundOne; i++) {//catch the first file that as the right extension and try to work with that one
-            iFile = listOfFiles[i];
-            if (iFile.isFile()) {
-                if (iFile.getName().endsWith(DB_FILE_EXTENSION)) ;
-                {
-                    foundOne = true;
-                }
-            }
-        }
-        if (foundOne) {
-            currFile = iFile;
-        }
-    }
 
     private void UpdateCurrFileLabel() {//update the current file name label
-        if (currFile == null) {
+        if (currDictionaryMapDb.getSelectedFileName() == null) {
             LabelcurrFile.setText("No file selected");
         } else {
-            LabelcurrFile.setText("Current file: " + currFile.getName());
+            LabelcurrFile.setText("Current file: " + currDictionaryMapDb.getSelectedFileName());
         }
     }
 
 
     private boolean SearchInTable(String key) {
         boolean result = true;
-        if (currDictionary.getEntry(key) != null) {
-            TableView.getSelectionModel().select(currDictionary.getEntry(key));//find the entry and choose it
-            TableView.scrollTo(currDictionary.getEntry(key));//go to found entry
+        DictionaryEntry found = null;
+        if ((found = currDictionaryMapDb.getEntry(key)) != null) {
+            TableView.getSelectionModel().select(found);//find the entry and choose it
+            TableView.scrollTo(found);//go to found entry
         } else
             result = false;
-
         return result;
     }
 }
--- src/DictionaryMapDb.java
+++ src/DictionaryMapDb.java
@@ -1,2 +1,224 @@
-package PACKAGE_NAME;public class DictionaryMapDb {
+import javafx.stage.FileChooser;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This is a singleton class that handles communication with the current DictionaryMap and files
+ */
+public class DictionaryMapDb {
+
+    private static DictionaryMapDb DictionaryMapDb_instance = null;
+
+    private DictionaryMap dictionaryMap = null;
+    private File currentFile = null;
+    private final String DB_Dir_PATH = (System.getProperty("user.dir") + "\\Db"); //the Db directory path
+    private final String DB_FILE_EXTENSION = ".mydb";//the db files extension
+
+
+    public static DictionaryMapDb getInstance() {
+        if (DictionaryMapDb_instance == null)
+            DictionaryMapDb_instance = new DictionaryMapDb();
+        return DictionaryMapDb_instance;
+    }
+
+
+    private DictionaryMapDb() {
+        chooseInitialFile();
+        DictionaryMap temp = loadDictionaryFromFile(currentFile);
+        if (temp != null)
+            dictionaryMap = temp;// load the dictionary from the file selected
+        else
+            dictionaryMap = new DictionaryMap();
+    }
+
+
+    private void chooseFile() {
+        FileChooser fileChooser = new FileChooser();//using preset file chooser
+        fileChooser.setTitle("Choose Db File from this folder only!");
+
+        File userDirectory = new File(DB_Dir_PATH);
+        fileChooser.setInitialDirectory(userDirectory);
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            DictionaryMap map = loadDictionaryFromFile(selectedFile);
+            if (map != null) {
+                currentFile = selectedFile;
+                dictionaryMap = map;
+            }
+
+        }
+        if (dictionaryMap == null)
+            dictionaryMap = new DictionaryMap();
+    }
+
+
+    private DictionaryMap loadDictionaryFromFile(File file) {
+        DictionaryMap result = null; //the new dictionary map to work with
+        FileInputStream fileIn = null;
+        ObjectInputStream objectIn = null;
+        if (file != null) {
+            try {//try to open and read from the file
+                fileIn = new FileInputStream(file);
+                objectIn = new ObjectInputStream(fileIn);
+
+                result = (DictionaryMap) objectIn.readObject();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Cannot load Dictionary from file! file name: " + file.getName());
+                result = null;
+            } finally {
+
+                try {//try to close file related objects
+                    objectIn.close();
+                } catch (Exception e) {
+                }
+                try {
+                    fileIn.close();
+                } catch (Exception e) {
+                }
+            }
+        } else
+            result = null;
+        return result;
+    }
+
+
+    public boolean chooseFileFromDir() {
+        boolean newFileChosen = false;
+
+        FileChooser fileChooser = new FileChooser();//using preset file chooser
+        fileChooser.setTitle("Choose Db File from this folder only!");
+        File userDirectory = new File(DB_Dir_PATH);
+        fileChooser.setInitialDirectory(userDirectory);
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            DictionaryMap map = loadDictionaryFromFile(selectedFile);
+            if (map != null) {
+                currentFile = selectedFile;
+                dictionaryMap = map;
+                newFileChosen = true;
+            } else {
+                if (dictionaryMap == null)
+                    dictionaryMap = new DictionaryMap();
+            }
+        }
+        return newFileChosen;
+    }
+
+
+    private void chooseInitialFile() {// called on startup, and chooses first file
+        File folder = new File(DB_Dir_PATH);
+        File[] listOfFiles = folder.listFiles();
+        File iFile = null;
+        boolean foundOne = false;
+        if (listOfFiles != null) {
+            for (int i = 0; i < listOfFiles.length && !foundOne; i++) {//catch the first file that as the right extension and try to work with that one
+                iFile = listOfFiles[i];
+                if (iFile.isFile()) {
+                    if (iFile.getName().endsWith(DB_FILE_EXTENSION)) ;
+                    {
+                        foundOne = true;
+                    }
+                }
+            }
+            if (foundOne) {
+                currentFile = iFile;
+            }
+        }
+    }
+
+
+    public boolean addEntry(DictionaryEntry newEntry) {
+        boolean isAdded = false;
+        if (dictionaryMap != null && newEntry != null) {
+            dictionaryMap.insert(newEntry);//insert a new object to the map and reload table
+            isAdded = true;
+        }
+        return isAdded;
+    }
+
+    public boolean removeEntry(DictionaryEntry newEntry) {
+        boolean isRemoved = false;
+        if (dictionaryMap != null && newEntry != null) {
+            dictionaryMap.remove(newEntry.getKey());//insert a new object to the map and reload table
+            isRemoved = true;
+        }
+        return isRemoved;
+    }
+
+    public boolean removeEntry(String key) {
+        boolean isRemoved = false;
+        if (dictionaryMap != null) {
+            dictionaryMap.remove(key);//insert a new object to the map and reload table
+            isRemoved = true;
+        }
+        return isRemoved;
+    }
+
+    public boolean EditEntry(DictionaryEntry newEntry) {
+        boolean isEdited = false;
+        if (dictionaryMap != null && newEntry != null) {
+            dictionaryMap.getEntry(newEntry.getKey()).setValue(newEntry.getValue());
+            isEdited = true;
+        }
+        return isEdited;
+    }
+
+    public String getSelectedFileName() {
+        if (currentFile != null)
+            return currentFile.getName();
+        return null;
+    }
+
+    private void SaveTofile(File file) {
+        if (file == null) {
+            file = currentFile;
+        }
+        try {
+            if (!file.equals(currentFile))
+                file.createNewFile();  //creates a new file
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(dictionaryMap);
+            oos.close();
+            currentFile = file;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Cannot save file!");
+        }
+    }
+
+    public void saveToCurrFile() {
+        if (currentFile != null)
+            SaveTofile(currentFile);
+    }
+
+    public void SaveToNewFile(String fileName) {
+        File file = new File(DB_Dir_PATH + "\\" + fileName + DB_FILE_EXTENSION); //initializing File object, and passing path as argument
+        SaveTofile(file);
+    }
+
+    public DictionaryEntry getEntry(String key) {
+        if (dictionaryMap != null)
+            return dictionaryMap.getEntry(key);
+        return null;
+    }
+
+    public DictionaryEntry getEntry(int index) {
+        if (dictionaryMap != null)
+            return dictionaryMap.getEntry(index);
+        return null;
+    }
+
+    public Set<Map.Entry<String, DictionaryEntry>> entrySet() {
+        if (dictionaryMap != null)
+            return dictionaryMap.entrySet();
+        return null;
+    }
+
+
 }
+
+
+
